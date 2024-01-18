058d24e031ef9d03eac008f7efb70bcda5d73b41
Bricret <brian.rico378@gmail.com>
2024-01-18 10:46:11 -0600

    Refactor NavBar component and add currency conversion rate

--- src/app/UI/dashboard/nav-bar.tsx
+++ src/app/UI/dashboard/nav-bar.tsx
@@ -13,31 +13,32 @@ export default async function NavBar({ title, site } : { title: string, site?: b
     const Rol = await FetchUnicRols(Number(session?.user?.image?.toString()));
 
     return (
-        <div >
-            <div className="flex flex-row justify-between items-center py-2 mb-10 mr-7 md:mr-0">
-                <div className="flex flex-row items-center gap-2">
-                    <div className="w-8 h-8 flex flex-wrap bg-secundary rounded-full"></div>
-                    <p 
-                        className={`text-sm md:text-xl font-semibold cursor-default ${ fonts.merriweather.className }`}
-                    >
-                        {title}
-                        {
-                            site ? 
-                            <span className="text-secundary font-bold">{ session?.user?.name }</span>
-                            : null  
-                        
-                        } 
-                    </p>
-                    <Image 
-                        src="/logo-unic.png"
-                        width={ 60 }
-                        height={ 60 }
-                        alt="Logo"
-                        className="object-contain hidden md:block w-auto h-auto"
-                    />
-                </div>
-                    <UserMenu username={ session?.user?.name || "" } rol={ Rol.nombre || "" }  />
+    <div className="flex flex-row justify-between items-center mb-10 py-2 mr-7 md:mr-0">
+        <div className="flex flex-col">
+            <div className="flex items-center">
+                <div className="w-8 h-8 flex flex-wrap bg-secundary rounded-full"></div>
+                <p 
+                    className={`text-sm md:text-xl font-semibold cursor-default px-2 ${ fonts.merriweather.className }`}
+                >
+                    {title}
+                    {
+                        site ? 
+                        <span className="text-secundary font-bold">{ session?.user?.name }</span>
+                        : null  
+                    
+                    } 
+                </p>
+                <Image 
+                    src="/logo-unic.png"
+                    width={ 60 }
+                    height={ 60 }
+                    alt="Logo"
+                    className="object-contain hidden md:block w-auto h-auto"
+                />
             </div>
+            <p className="text-lg text-black/40 font-semibold">Cambio: {process.env.NEXT_PUBLIC_CONVERSION_RATE}</p>
         </div>
+            <UserMenu username={ session?.user?.name || "" } rol={ Rol.nombre || "" }  />
+    </div>
     )
 }
