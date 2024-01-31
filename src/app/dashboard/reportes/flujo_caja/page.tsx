c4c522d8f40f9516ee2b6021f41d1a98c238d2e1
Bricret <brian.rico378@gmail.com>
2024-01-31 10:55:48 -0600

    content divide in two page's

--- src/app/dashboard/reportes/flujo_caja/page.tsx
+++ src/app/dashboard/reportes/flujo_caja/page.tsx
@@ -1,8 +1,34 @@
+import { LatestInventorySkeleton } from '@/app/UI/Skeleton';
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
 
 
-export default function FlujoPage() {
+const DynamicTableFlowCash = dynamic(() => import('@/app/UI/reportes/TableFlowCash'), {
+    loading: () => <LatestInventorySkeleton />,
+});
+
+export default function FlujoPage({ searchParams } : { searchParams?: {
+    dataForPage?: string,
+    pageFlow?: string
+    startDateFlow?: string,
+    endDateFlow?: string,
+
+}}) {
+
+    const dataParamsFlowCash = {
+        itemsForPage : Number(searchParams?.dataForPage) || 5,
+        currentPage : Number(searchParams?.pageFlow) || 1,
+        startDate : searchParams?.startDateFlow || "",
+        endDate : searchParams?.endDateFlow || ""
+    }
+
 
     return (
-        <h1>hola</h1>
+        <section>
+            <h1 className="text-2xl font-semibold text-gray-600 mb-4 ">Flujos de Caja</h1>
+            <Suspense fallback={ <LatestInventorySkeleton /> }>
+                <DynamicTableFlowCash dataParams={dataParamsFlowCash} />
+            </Suspense>
+        </section>
     )
 }
--- src/app/dashboard/reportes/page.tsx
+++ src/app/dashboard/reportes/page.tsx
@@ -1,30 +1,18 @@
 import dynamic from 'next/dynamic';
 import { LatestInventorySkeleton } from "@/app/UI/Skeleton";
-import NavBar from "@/app/UI/dashboard/nav-bar";
-import { Metadata } from "next"
 import { Suspense } from "react";
-
-
 
 const DynamicTableInvoices = dynamic(() => import('@/app/UI/reportes/TableInvoices'), {
   loading: () => <LatestInventorySkeleton />,
 });
 
-const DynamicTableFlowCash = dynamic(() => import('@/app/UI/reportes/TableFlowCash'), {
-  loading: () => <LatestInventorySkeleton />,
-});
-
 export default function ReportsPage({ searchParams } : { searchParams?: {
     query?: string,
-    dataForPage?: string,
     page?: string,
     startDate?: string,
     endDate?: string,
-    startDateFlow?: string,
-    endDateFlow?: string,
-    pageFlow?: string
-
-} }) {
+    dataForPage?: string,
+    } }) {
 
     const dataParamsInvoice = {
         itemsForPage : Number(searchParams?.dataForPage) || 5,
@@ -34,31 +22,14 @@ export default function ReportsPage({ searchParams } : { searchParams?: {
         endDate : searchParams?.endDate || ""
     }
 
-    const dataParamsFlowCash = {
-        itemsForPage : Number(searchParams?.dataForPage) || 5,
-        currentPage : Number(searchParams?.pageFlow) || 1,
-        startDate : searchParams?.startDateFlow || "",
-        endDate : searchParams?.endDateFlow || ""
-    }
-
     return (
-    <>
-        <main>
-            <section className="mb-12">
-                <h1 className="text-2xl font-semibold text-gray-600 mb-4">Facturas Totales</h1>
-                <Suspense fallback={ <LatestInventorySkeleton /> }>
-                    <DynamicTableInvoices dataParams={dataParamsInvoice} />
-                </Suspense>
-            </section>
-
-            <section>
-                <hr className="border-t-2 border-t-secundary/30 pb-4" />
-                <h1 className="text-2xl font-semibold text-gray-600 mb-4 ">Flujos de Caja</h1>
-                <Suspense fallback={ <LatestInventorySkeleton /> }>
-                    <DynamicTableFlowCash dataParams={dataParamsFlowCash} />
-                </Suspense>
-            </section>
-        </main>
-    </>
+    <main>
+        <section className="mb-12">
+            <h1 className="text-2xl font-semibold text-gray-600 mb-4">Facturas Totales</h1>
+            <Suspense fallback={ <LatestInventorySkeleton /> }>
+                <DynamicTableInvoices dataParams={dataParamsInvoice} />
+            </Suspense>
+        </section>
+    </main>
     )
 }
